a711bb0b0951be6f6a31871a1e12be14dd83a5ce
Tayfun Sur <tayfunsur@gmail.com>
2022-10-26 21:39:52 +0300

    tests: Add bundle_type in txns and add a unit test

--- flashbots-transactions-detector/src/agent.spec.js
+++ flashbots-transactions-detector/src/agent.spec.js
@@ -1,13 +1,18 @@
 const { FindingType, FindingSeverity, Finding } = require("forta-agent");
 const axios = require("axios");
 const { provideHandleBlock, resetLastBlockNumber } = require("./agent");
+const { createAddress } = require("forta-agent-tools");
 
-const from1 = "0xfrom1";
-const to1 = "0xto1";
-const from2 = "0xfrom2";
-const to2 = "0xto2";
-const from3 = "0xfrom3";
-const to3 = "0xto3";
+const from0 = createAddress("0xf0");
+const to0 = createAddress("0xb0");
+const from1 = createAddress("0xf1");
+const to1 = createAddress("0xb1");
+const from2 = createAddress("0xf2");
+const to2 = createAddress("0xb2");
+const from3 = createAddress("0xf3");
+const to3 = createAddress("0xb3");
+const from4 = createAddress("0xf4");
+const to4 = createAddress("0xb4");
 
 const block1 = {
   block_number: 1,
@@ -16,6 +21,7 @@ const block1 = {
       eoa_address: from1,
       to_address: to1,
       transaction_hash: "0x1",
+      bundle_type: "flashbots",
     },
   ],
 };
@@ -27,11 +33,43 @@ const block2 = {
       eoa_address: from2,
       to_address: to2,
       transaction_hash: "0x2",
+      bundle_type: "flashbots",
     },
     {
       eoa_address: from3,
       to_address: to3,
       transaction_hash: "0x3",
+      bundle_type: "flashbots",
+    },
+  ],
+};
+
+const block3 = {
+  block_number: 3,
+  transactions: [
+    {
+      eoa_address: from0,
+      to_address: to0,
+      transaction_hash: "0x5",
+      bundle_type: "mempool",
+    },
+    {
+      eoa_address: from2,
+      to_address: to2,
+      transaction_hash: "0x2",
+      bundle_type: "flashbots",
+    },
+    {
+      eoa_address: from3,
+      to_address: to3,
+      transaction_hash: "0x3",
+      bundle_type: "flashbots",
+    },
+    {
+      eoa_address: from4,
+      to_address: to4,
+      transaction_hash: "0x4",
+      bundle_type: "mempool",
     },
   ],
 };
@@ -141,5 +179,47 @@ describe("flashbots transactions detection bot", () => {
       }),
     ]);
     expect(mockGetTransactionReceipt).toHaveBeenCalledTimes(3);
+  });
+
+  it("should filter out 2 mempool transactions and return 2 findings for the flashbots transactions", async () => {
+    const response = { data: { blocks: [block3] } };
+    const logs = [{ address: to2 }];
+    const logs2 = [{ address: to3 }];
+    mockGetTransactionReceipt.mockResolvedValueOnce({ logs: logs });
+    mockGetTransactionReceipt.mockResolvedValueOnce({ logs: logs2 });
+    axios.get.mockResolvedValueOnce(response);
+    const findings = await handleBlock();
+
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        name: "Flashbots transactions",
+        description: `${from2} interacted with ${to2} in a flashbots transaction`,
+        alertId: "FLASHBOTS-TRANSACTIONS",
+        severity: FindingSeverity.Low,
+        type: FindingType.Info,
+        addresses: [to2],
+        metadata: {
+          from: from2,
+          to: to2,
+          hash: "0x2",
+          blockNumber: block3.block_number,
+        },
+      }),
+      Finding.fromObject({
+        name: "Flashbots transactions",
+        description: `${from3} interacted with ${to3} in a flashbots transaction`,
+        alertId: "FLASHBOTS-TRANSACTIONS",
+        severity: FindingSeverity.Low,
+        type: FindingType.Info,
+        addresses: [to3],
+        metadata: {
+          from: from3,
+          to: to3,
+          hash: "0x3",
+          blockNumber: block3.block_number,
+        },
+      }),
+    ]);
+    expect(mockGetTransactionReceipt).toHaveBeenCalledTimes(2);
   });
 });
